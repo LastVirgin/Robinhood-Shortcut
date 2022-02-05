dbcb1da5dd32e81511a6de666655cda1bd1cbb19
LastVirgin <99066128+LastVirgin@users.noreply.github.com>
2022-02-04 23:08:40 -0800

    Add files via upload

---
+++ Robinhood-Shortcut.py
@@ -0,0 +1,11 @@
+from doctest import master
+import webbrowser
+import subprocess
+#firefox_path = r"C:\Program Files\Mozilla Firefox\firefox.exe"
+#webbrowser.register('firefox', None, 
+#                   webbrowser.BackgroundBrowser(firefox_path))
+#entry = input()
+#phrase = entry
+#url = phrase
+#print ("browser" +" "+ phrase)
+webbrowser.open("https://www.robinhood.com")
